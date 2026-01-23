5b757101df400b995c168e59c077940cfd1facdd
danir28 <d.r7428331@gmail.com>
2026-01-22 21:11:45 -0300

    Simplifica modelo: alumnos manejados con diccionarios

--- src/batch_alumnos/models/alumnos.py
+++ src/batch_alumnos/models/alumnos.py
@@ -4,11 +4,6 @@ No pide input
 No imprime nada"""
 #Representa el concepto de alumno o alumnos.
 
-class Alumno:
-    def __init__(self, nombre: str, nota: float):
-        self.nombre = nombre
-        self.nota = nota
-        
 def crear_alumno(nombre, nota):
     return {
         'nombre': nombre,
